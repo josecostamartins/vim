904a6486749dd29e1e3843812cfcec2610f760df
Jose Augusto Costa Martins Jr <josecostamartins@gmail.com>
2014-03-30 17:13:18 -0300

    changed initial_config.sh to be more adequate to both mac and linux (still need to test)

--- initial_config.sh
+++ initial_config.sh
@@ -1,43 +1,100 @@
 #!/bin/bash
 
+FONTS=~/.vim/fonts/*
+PATH_TO_APPEND=( "PATH=/usr/local/bin:\$PATH" "export PATH" "export WORKON_HOME=~/.virtualenvs" "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python" "source /usr/local/bin/virtualenvwrapper.sh" )
+SO="none"
+MINPIP_VERSION=1.4
 
+
+########################################################################
+#                                                                      #
+#             Checking for permissions and prerequisits                #
+#                                                                      #
+########################################################################
+
+#utility function to check if some components are installed
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
+# check if current user can use sudo, it is necessary to install pip
+# and other stuff
+has_root_access=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+if [ ${has_root_access} -gt 0 ]; then
+    echo "OK, you have sudo permissions"
+else
+    echo "You need sudo permission to run this file"
+    exit 1
+fi
+
 if [ "$(uname)" == "Darwin" ]; then
+    SO="mac"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    SO="linux"
+else
+    echo "This script only works on Mac OS X and Linux Distributions"
+    echo "Exiting..."
+    exit 1
+fi
+
+
+if [ "$SO" == "mac" ]; then
+    if command_exists brew; then
+        echo "OK, you have homebrew installed"
+    else
+        echo "You do not have homebrew installed, please install it"
+        exit 1
+    fi
+fi
+
+if command_exists pip; then
+    echo "OK, you have pip installed"
+    SYSPIP_VERSION=$(pip --version | awk '{ split($2,a,"."); print a[1]"."a[2]; }')
+    comp=$(echo $SYSPIP_VERSION '>=' $MINPIP_VERSION | bc -l)
+    if [ $comp -gt 0 ]; then
+        echo "OK, you have pip >= 1.4"
+    else
+        echo "You do not have pip >= 1.4, please upgrade"
+        echo "exiting..."
+        exit 1
+    fi
+fi
+
+########################################################################
+#                                                                      #
+#                Set some paths and install dependencies               #
+#                                                                      #
+########################################################################
+
+if [ "$SO" == "mac" ]; then
+
+    echo "Installing vim configurations for Mac OS X"
+
     BASH_PROFILE=~/.bash_profile
     FONTS_PATH=~/Library/Fonts
-    # Do something under Mac OS X platform        
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    pip install flake8
+    pip install jedi
+    pip install virtualenv
+    pip install virtualenvwrapper
+    brew install node
+elif [ "$SO" == "linux" ]; then
+
+    echo "Installing vim configurations for Linux"
+
     BASH_PROFILE=~/.bashrc
     FONTS_PATH=~/.fonts
     sudo apt-get install vim
     sudo apt-get install python-pip python-dev build-essential
-    # Do something under Linux platform
-fi
-
-FONTS=~/.vim/fonts/*
-
-PATH_TO_APPEND=( "PATH=/usr/local/bin:\$PATH" "export PATH" "export WORKON_HOME=~/.virtualenvs" "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python" "source /usr/local/bin/virtualenvwrapper.sh" )
-
-if command_exists pip; then
-    echo "Install flake8, jedi, virtualenv and virtualenvwrapper..."
-    pip install flake8
-    pip install jedi
-    pip install virtualenvwrapper
+    sudo pip install flake8
+    sudo pip install jedi
+    sudo pip install virtualenv
+    sudo pip install virtualenvwrapper
+    sudo apt-get install node
 else
-    echo "You do not have pip"
-    echo "You should install python using homebrew"
+    echo "Unfortunately, this script only works under Mac OS X or Linux"
+    exit 1
 fi
 
-if command_exists brew; then
-    echo "Installing node.js..."
-    brew install node
-else
-    echo "could not install node"
-    echo "install homebrew first"
-fi
 
 if [ ! -f "$BASH_PROFILE" ]; then
     echo "Creating $BASH_PROFILE""..."
@@ -65,4 +122,3 @@ done
 #echo "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python" >> ~/.bash_profile
 #echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bash_profile
 #endif
-
